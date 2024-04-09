08d66957603f84788cec96aaa068ea307aab6359
luke-hanwook <gksdnr89@gmail.com>
2024-04-09 15:20:43 +0900

    pre-rendering, data fetching 연습

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,35 @@
 import Head from "next/head";
 import Layout, { siteTitle } from "../components/layout";
 import utilStyles from "../styles/utils.module.css";
+// import { getSortedPostsData } from "../lib/posts";
+// import { useState, useEffect } from "react";
 
-export default function Home() {
+// ssg, ssr
+// csr - fs는 서버사이드에서만 가능하여 => api routes 활용
+// ssg, ssr에서 api routes 시 주소는 절대 경로 표시
+
+export async function getStaticProps() {
+  // const allPostsData = getSortedPostsData();
+
+  const res = await fetch("http://localhost:3000/api/posts");
+  const { allPostsData } = await res.json();
+
+  return {
+    props: {
+      allPostsData,
+    },
+  };
+}
+
+export default function Home({ allPostsData }) {
+  // const [allPostsData, setAllPostsData] = useState([]);
+
+  // useEffect(() => {
+  //   fetch("/api/posts")
+  //     .then((res) => res.json())
+  //     .then((json) => setAllPostsData(json.allPostsData));
+  // }, []);
+
   return (
     <Layout home>
       <Head>
@@ -14,6 +41,20 @@ export default function Home() {
           (This is a sample website - you’ll be building a site like this on{" "}
           <a href="https://nextjs.org/learn">our Next.js tutorial</a>.)
         </p>
+      </section>
+      <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
+        <h2 className={utilStyles.headingLg}>Blog</h2>
+        <ul className={utilStyles.list}>
+          {allPostsData.map(({ id, date, title }) => (
+            <li className={utilStyles.listItem} key={id}>
+              {title}
+              <br />
+              {id}
+              <br />
+              {date}
+            </li>
+          ))}
+        </ul>
       </section>
     </Layout>
   );
