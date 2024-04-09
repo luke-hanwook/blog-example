144382f697dd4ac96d1d957252e76cbaa9c540aa
luke-hanwook <gksdnr89@gmail.com>
2024-04-09 11:34:54 +0900

    Link 태그 연습 - client side navigate & prefetching

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,6 @@
-import Head from 'next/head';
-import styles from '../styles/Home.module.css';
+import Head from "next/head";
+import styles from "../styles/Home.module.css";
+import Link from "next/link";
 
 export default function Home() {
   return (
@@ -10,6 +11,29 @@ export default function Home() {
       </Head>
 
       <main>
+        <h1 className={styles.title}>
+          Read this post{" "}
+          {/*
+              Link를 사용하면...?
+              client side navigate
+              - html 새로운 로드 없이 특정 js 상에서 page component만 교체
+
+              next.js는 automatic code spliting을 제공
+              - 특정 페이지를 접근할 때 해당 페이지를 그릴 때 필요한 chunk만 로드
+              - 페이지 이동을 할때 목적지 페이지에 필요한 chunk만 로드
+
+              prefetch
+              - viewport에 Link태그가 노출되었을 때 href로 연결된 페이지의 chunk를 로드
+
+              이를 통해 성능이 최적화 됨.
+            */}
+          <Link href={"/posts/first-post"} prefetch={false}>
+            첫번째 글
+          </Link>
+        </h1>
+        <h1 className={styles.title}>
+          Read this post (a tag) <a href="/posts/fitst-post">첫번째 글</a>
+        </h1>
         <h1 className={styles.title}>
           Welcome to <a href="https://nextjs.org">Next.js!</a>
         </h1>
@@ -48,14 +72,16 @@ export default function Home() {
           </a>
         </div>
       </main>
-
+      <Link href={"/posts/second-post"} prefetch={false}>
+        두번째 글
+      </Link>
       <footer>
         <a
           href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Powered by{' '}
+          Powered by{" "}
           <img src="/vercel.svg" alt="Vercel" className={styles.logo} />
         </a>
       </footer>
