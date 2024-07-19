0469565904d0800060f4552bfa941c186c0de26b
luke-hanwook <gksdnr89@gmail.com>
2024-07-19 15:41:53 +0900

    next-sitemap 으로 사이트맵 추가

---
+++ next-sitemap.config.js
@@ -0,0 +1,6 @@
+/** @type {import('next-sitemap').IConfig} */
+module.exports = {
+  siteUrl: "https://blog-example-sandy.vercel.app",
+  generateRobotsTxt: true, // (optional)
+  // ...other options
+};
